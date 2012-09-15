d7abffbad37eb864b3f12c4f9fc3721c1fb34cea
keijsuzu <scenery.recovers@live.jp>
2012-09-15 22:57:52 +0900

    ts以外もエンコードできるように。

--- optimum_hbc_enc.pl
+++ optimum_hbc_enc.pl
@@ -7,7 +7,6 @@
 
 my $workdir = "${0}_work";         # 作業ディレクトリ
 my $pfile = "$workdir/running.pid"; # 重複でプロセスが動かないように。
-my $rundum_time = 10;              # ランダムの最大時間 -1秒
 my $encode_soft_path = 'C:"Program Files (x86)"\\Handbrake\\HandBrakeCLI.exe';
 
 # 強制終了時にpfileを消すようにする
@@ -27,23 +26,18 @@ unless ( -d $workdir ) {
 # 複数ファイルを引数にした場合順番に処理
 warn @ARGV,"\n";
 while (@ARGV) {
-    if ( -f $pfile ) {
-        sleep int(rand 10);
-    } else {
-        # エンコードするファイルを表示
-        warn @ARGV,"\n";
-        print "open\n";
-        open(F, "> $pfile") or next;
-        flock(F, 6) or next;
-        print F "$ARGV[0] encode start\n";
-        $inputfile = $ARGV[0];
-        $outputfile = $ARGV[0];
-        $outputfile =~ s/\.ts$/.mp4/g;
-        $other_options = "-e x264 --keep-display-aspect --no-dvdnav --cfr";
-        system("$encode_soft_path -i \"$inputfile\" -o \"$outputfile\" $other_options");
-        flock(F, 8);
-        close(F);
-        unlink $pfile;
-        shift @ARGV;
-    }
+    # エンコードするファイルを表示
+    warn @ARGV,"\n";
+    print "open\n";
+    open(F, "> $pfile") or next;
+    flock(F, 2) or next;
+    print F "$ARGV[0] encode start\n";
+    $inputfile = $ARGV[0];
+    $outputfile = $ARGV[0];
+    $outputfile =~ s/\.[^.]+$/.mp4/g; # 拡張子変換
+    $other_options = "--preset=Normal";
+    system("$encode_soft_path -i \"$inputfile\" -o \"$outputfile\" $other_options");
+    close(F); # 自動でロックも解除する
+    unlink $pfile;
+    shift @ARGV;
 }
